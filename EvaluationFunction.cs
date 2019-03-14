c3c04bce22c5ee14f3de215a3d3f754e1df5b171
RicardoJMPaiva <43242841+RicardoJMPaiva@users.noreply.github.com>
2019-03-14 21:19:41 +0000

    Add files via upload
    Só mm p adicionar tudo

---
+++ EvaluationFunction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EvaluationFunction
+{
+    // Do the logic to evaluate the state of the game !
+    public float evaluate(State s)
+    {
+        
+
+        return 0;
+    }
+}
