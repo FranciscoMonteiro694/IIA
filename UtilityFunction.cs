c51fcf3a5e36f49a2f5aaaaca664713ce5fa58e3
RicardoJMPaiva <43242841+RicardoJMPaiva@users.noreply.github.com>
2019-03-18 16:11:59 +0000

    Update UtilityFunction.cs

--- UtilityFunction.cs
+++ UtilityFunction.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System;
 
@@ -7,9 +7,22 @@ public class UtilityFunction
 
     public float evaluate(State s)
     {
-        /////////////////
-        // You should implement these
-        /////////////////
-        return 0;
+        float acum1 = 0;
+        if(s.AdversaryUnits.Count == 0)
+        {
+            foreach (Unit t in s.PlayersUnits)
+            {
+                acum1 += t.hp * t.pontos;
+            }
+            return acum1;
+        }
+        else
+        {
+            foreach (Unit t in s.AdversaryUnits)
+            {
+                acum1 += t.hp * t.pontos;
+            }
+            return -acum1;
+        }
     }
 }
