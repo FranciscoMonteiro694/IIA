d50a5fc73591d236aaa18f4d2ffc08a85814d8d5
FranciscoMonteiro694 <34212758+FranciscoMonteiro694@users.noreply.github.com>
2019-04-02 15:33:59 +0100

    Add files via upload

---
+++ UtilityFunction.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class UtilityFunction
+{
+
+    public float evaluate(State s)
+    {
+        float acum1 = 0;
+        if (s.AdversaryUnits.Count == 0)
+        {
+            foreach (Unit t in s.PlayersUnits)
+            {
+                acum1 += t.hp * t.pontos;
+            }
+            return acum1;
+        }
+        else
+        {
+            foreach (Unit t in s.AdversaryUnits)
+            {
+                acum1 += t.hp * t.pontos;
+            }
+            return -acum1;
+        }
+    }
+}
